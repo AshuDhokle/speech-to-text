0a5a91f7ca3f6f93eb4de5620dcd9456749bd897
Ashutosh45c <dhokle100@gmail.com>
2025-02-07 23:45:19 +0530

    initial commit

---
+++ src/app/api/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.API_KEY });
+
+export async function POST(req: NextRequest) {
+  try {
+    // Extracting file from the request body
+    const data = await req.formData();
+    const theFile: File | null = data.get("file") as unknown as File;
+    
+    const formData = new FormData();
+    formData.append("file", theFile);
+    formData.append("model", "whisper-1");
+
+    const url = "https://chatgpt-42.p.rapidapi.com/whisperv3";
+    const options = {
+      method: "POST",
+      headers: {
+        "x-rapidapi-key": "65678eeabamsh5e85a0f267aa77ep1dcce1jsne84310329fb0",
+        "x-rapidapi-host": "chatgpt-42.p.rapidapi.com"
+      },
+      body: formData,
+    };
+
+    const response = await fetch(
+      url, options
+    );
+
+    const body = await response.json();
+    return NextResponse.json({ output: body }, { status: 200 })
+
+  } catch (error) {
+    console.error("Error processing audio:", error);
+    return NextResponse.json(
+      { error: "Something went wrong. Please try again." },
+      { status: 500 }
+    );
+  }
+}
